6b9550c6f296be0d5552bd0751b7036e2a21e062
Santosh Kumar <santoshkumar23kky@gmail.com>
2024-07-03 13:52:20 +0530

    automaticBanner

--- script.js
+++ script.js
@@ -11,5 +11,49 @@ function horizontalScrollAnimation() {
   });
 }
 
+function automaticBanner() {
+  var tl = gsap.timeline({
+    repeat: -1,
+  });
+  tl.to(
+    ".banner-img",
+    {
+      transform: "translateX(-100%)",
+      duration: 1,
+      ease: "power4",
+    },
+    "+=3"
+  );
+  tl.to(
+    ".banner-img",
+    {
+      transform: "translateX(-200%)",
+      duration: 1,
+      ease: "power4",
+    },
+    "+=3"
+  );
+  tl.to(
+    ".banner-img",
+    {
+      transform: "translateX(-300%)",
+      duration: 1,
+      ease: "power4",
+    },
+    "+=3"
+  );
+  tl.to(
+    ".banner-img",
+    {
+      transform: "translateX(-400%)",
+      duration: 1,
+      ease: "power4",
+    },
+    "+=3"
+  );
+}
+
+
 
 horizontalScrollAnimation();
+automaticBanner();
